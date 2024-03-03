47e62c4726c7e1f20ed7cbf9ce4cb601fdb847d3
David Castellanos <david.ingsof@gmail.com>
2024-03-03 09:44:53 -0500

    se implementan consultar propietario y otros ajustes

--- Propietarios_Mascotas/urls.py
+++ Propietarios_Mascotas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('consultorio/', views.consultorio, name='consultorio'),
+    path('consultorio/', views.listar_propietarios, name='listar_propietarios'),
     path('registrar_propietario/', views.registrar_propietario),
+    path('Perfil_propietario/<int:documento>/', views.Perfil_Propietario),
+    path('registrar_mascota/', views.Registrar_Mascota),
 ]
 
--- Propietarios_Mascotas/views.py
+++ Propietarios_Mascotas/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Propietario
-
+from .models import Propietario, Mascota, EstadoReproductivo
 # Create your views here.
 
-def consultorio(request):
-    return render(request, 'consultorio.html')
 
 def registrar_propietario(request):
     documento = request.POST.get('txtDocumentoP')
@@ -17,7 +14,50 @@ def registrar_propietario(request):
     telefonoEmer = request.POST.get('txtTelE')
     
     propietario = Propietario.objects.create(documento=documento, nombres=nombres, apellidos=apellidos, direccion=direccion, email=email, telefono=telefono, nombreEmer=nombreEmer, telefonoEmer=telefonoEmer)
+    propietario = Propietario.objects.get(documento=documento)
     
-    return redirect('consultorio')
-      
-      
+    
+    return render(request, 'Perfil_propietario.html', {'propietario':propietario} )
+
+def listar_propietarios(request):
+    propietarios = Propietario.objects.all()
+    return render(request, 'consultorio.html', {'propietarios':propietarios})
+
+def Perfil_Propietario(request, documento):
+    estados = EstadoReproductivo.objects.all()
+    propietarios = Propietario.objects.all()
+    propietario = Propietario.objects.get(documento=documento)
+    return render(request, 'Perfil_propietario.html', {'propietario':propietario, 'estados': estados, 'propietarios':propietarios})
+
+def Registrar_Mascota(request):
+    
+    propietarios = Propietario.objects.all()
+    estados = EstadoReproductivo.objects.all()
+    
+    idMascota = request.POST.get('txtIdMascota')
+    foto = request.POST.get('txtFoto')
+    nombre = request.POST.get('txtNombreMascota')
+    especie = request.POST.get('txtEspecie')
+    raza = request.POST.get('txtRaza')
+    sexo = request.POST.get('txtSexo')
+    color = request.POST.get('txtColor')
+    edad = request.POST.get('txtEdad')
+    meses = request.POST.get('txtMeses')
+    fechaNacimiento = request.POST.get('txtFechaNacimiento')
+    peso = request.POST.get('txtPeso')
+    rh = request.POST.get('txtRH')
+    personalidad = request.POST.get('txtPersonalidad')
+    alimentacion = request.POST.get('txtAlimentacion')
+    cantidadAlimento = request.POST.get('txtCantidadAlimento')
+    frecuenciaAlimentacion = request.POST.get('txtFrecuenciaAlimentacion')
+    frecuenciaBano = request.POST.get('txtFrecuenciaBano')
+    antecedentesClinicos = request.POST.get('txtAntecedentesClinicos')
+    propietario = request.POST.get('txtPropietario')
+    Estados = request.POST.get('txtEstadoReproductivo')
+    
+    propietario = Propietario.objects.get(pk=propietario)
+    estados = EstadoReproductivo.objects.get(pk=Estados)
+
+   
+    mascota = Mascota.objects.create(idMascota=idMascota, foto=foto, nombre=nombre, especie=especie, raza=raza, sexo=sexo, color=color, edad=edad, meses=meses, fechaNacimiento=fechaNacimiento, peso=peso, rh=rh, personalidad=personalidad, alimentacion=alimentacion, cantidadAlimento=cantidadAlimento, frecuenciaAlimentacion=frecuenciaAlimentacion, frecuenciaBano=frecuenciaBano, antecedentesClinicos=antecedentesClinicos, propietario=propietario, EstadoReproductivo=estados)
+    return render(request, 'consultorio.html', {'propietario':propietario , 'estados': estados} )
