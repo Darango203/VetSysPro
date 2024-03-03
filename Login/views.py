beaec59e98d7be35b19ddc56126ce9b01e291185
Anderson0315 <anderc0315@gmail.com>
2024-03-03 18:12:33 -0500

    añade la funcionalidad del login

--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('usuario')
+        password = request.POST.get('clave')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redireccionar al usuario a la página de consultorio en caso de que el usuario este registrado
+            return redirect('listar_propietarios')
+        else:
+            # Manejar caso de credenciales inválidas en caso de que el usuario no este registrado
+            return render(request, 'login.html', {'error_message': 'Credenciales inválidas'})
+    else:
+        return render(request, 'login.html')
